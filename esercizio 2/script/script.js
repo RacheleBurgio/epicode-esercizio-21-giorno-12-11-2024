fc5bb16894573be2eef731eb58c75979aabca1cb
RacheleBurgio <rachele.burgio32@gmail.com>
2024-11-12 15:33:04 +0100

    rifatto tutto l'esercizio 2
    niente pensavo che bastava il timer e basta non avevo letto bene la consegna e quindi l'ho rifatto tutto da 0

--- esercizio 2/script/script.js
+++ esercizio 2/script/script.js
@@ -1,38 +1,13 @@
-let timerInterval
-let timeLeft = 60
-
-window.onload = () => {
-  const timerElement = document.getElementById('seconds')
-  const circle = document.querySelector('.progress-ring-circle')
-  const radius = circle.r.baseVal.value
-  const circonferenza = 2 * Math.PI * radius
-  circle.style.strokeDasharray = `${circonferenza} ${circonferenza}`
-  circle.style.strokeDashoffset = `${circonferenza}`
-
-  function setProgress(percent) {
-    const offset = circonferenza - (percent / 100) * circonferenza
-    circle.style.strokeDashoffset = offset
-  }
-
-  const resetTimer = () => {
-    clearInterval(timerInterval)
-    timeLeft = 60
-    const totalTime = timeLeft
-    timerElement.innerHTML = timeLeft
-    setProgress(100)
-
-    timerInterval = setInterval(() => {
-      if (timeLeft > 0) {
-        timeLeft--
-        timerElement.innerHTML = timeLeft
-
-        const progressPercentage = (timeLeft / totalTime) * 100
-        setProgress(progressPercentage)
-      } else {
-        clearInterval(timerInterval)
-      }
-    }, 1000)
-  }
-
-  resetTimer()
+if (sessionStorage.getItem('timer')) {
+  timer = parseInt(sessionStorage.getItem('timer'))
+} else {
+  timer = 0
 }
+
+function counter() {
+  timer++
+  document.getElementById('Timer').textContent = timer
+  sessionStorage.setItem('timer', timer)
+}
+
+setInterval(counter, 1000)
